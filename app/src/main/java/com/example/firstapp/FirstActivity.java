df4737b0d2fab88ade7a3cb1afc76a2be0dae6af
saljeshh <saljesh2020@gmail.com>
2023-04-06 08:39:44 +0545

    april 6, startActivity, onActivityrestul, setResult

--- app/src/main/java/com/example/firstapp/FirstActivity.java
+++ app/src/main/java/com/example/firstapp/FirstActivity.java
@@ -1,5 +1,6 @@
 package com.example.firstapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +14,7 @@ public class FirstActivity extends AppCompatActivity {
 
     // for button
     Button btn1, btn2, btn3;
-    TextView tv1,tv2;
+    TextView tv1,tv2,tv3;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +27,7 @@ public class FirstActivity extends AppCompatActivity {
 
         tv1=findViewById(R.id.tv_text1);
         tv2=findViewById(R.id.tv_text2);
+        tv3=findViewById(R.id.tv_text3);
 
         tv1.setText("This is textview example");
         tv2.setText(R.string.app_name);
@@ -53,5 +55,37 @@ public class FirstActivity extends AppCompatActivity {
                 startActivity(intnet);
             }
         });
+
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("DATA", "This is new activity result");
+                intent.putExtra("INTEGER_VALUE", 400);
+                startActivityForResult(intent, 30);
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == 30){
+                if(data != null){
+                    if(data.hasExtra("RESULT")){
+                        String str = data.getStringExtra("RESULT");
+                        tv3.setText(str);
+                    }
+                }
+            }
+        }
+//            else if (resultCode == Activity.RESULT_CANCELED){
+//                super.onActivityResult(requestCode, resultCode, data);
+//            }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/example/firstapp/SecondActivity.java
+++ app/src/main/java/com/example/firstapp/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.firstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -9,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SecondActivity extends AppCompatActivity {
     // make variables for textview
     TextView tv1, tv2;
+    Button btnBack;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +25,7 @@ public class SecondActivity extends AppCompatActivity {
         // receive data from FirstActivity
         tv1 = findViewById(R.id.tv_text1);
         tv2 = findViewById(R.id.tv_text2);
+        btnBack = findViewById(R.id.btn_back);
 
         if(getIntent().hasExtra("DATA")){
             tv1.setText(getIntent().getStringExtra("DATA"));
@@ -31,5 +36,17 @@ public class SecondActivity extends AppCompatActivity {
             // converting to string from int
             tv2.setText(String.valueOf(val));
         }
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // passing from child to parent
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("RESULT", "Successful resrult ok");
+
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        });
     }
 }
