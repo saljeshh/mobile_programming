eccb77d9fa8b7ce4cafaa88937fe4fe5f92e6f78
saljeshh <saljesh2020@gmail.com>
2023-05-04 07:43:53 +0545

    pushed for rohan , custom dialog

---
+++ app/src/main/java/com/example/firstapp/CustomDialogFragment.java
@@ -0,0 +1,20 @@
+package com.example.firstapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class CustomDialogFragment extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.custom_dialog_layout, container, false);
+
+        return view;
+    }
+}
